f76d6745be8a17b6bcd9d1926d0b6b945e1b00d9
jax1109 <jax1@rambler.ru>
2015-12-09 18:27:09 +0200

    hit

---
+++ src/com/javarush/test/level05/lesson12/home04/Solution.java
@@ -0,0 +1,28 @@
+package com.javarush.test.level05.lesson12.home04;
+
+/* Вывести на экран сегодняшнюю дату
+Вывести на экран текущую дату в аналогичном виде "21 02 2014".
+*/
+
+
+import java.util.Calendar;
+
+public class Solution
+{
+    public static void main(String[] args)
+    {
+
+        {
+            //Напишите тут ваш код
+            Calendar c = Calendar.getInstance();
+
+            int year = c.get(c.YEAR);
+            int month = c.get(c.MONTH);
+            int date = c.get(c.DATE);
+
+            month++;
+            System.out.println("0"+date + " " + month + " " + year);
+
+        }
+    }
+}
